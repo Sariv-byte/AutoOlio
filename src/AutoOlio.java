538fd119e785d1725efd953aa01c0e1d3bc7f150
Sariv-byte <112755988+Sariv-byte@users.noreply.github.com>
2022-12-03 21:35:02 +0200

    Ensimmainen versio

--- src/AutoOlio.java
+++ src/AutoOlio.java
@@ -2,8 +2,31 @@
 public class AutoOlio {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
+} //Main luokan lopetus
+
+class Auto {
+	//ominaisuudet
+	
+	String merkki;
+	String malli;
+	int bensanMaara=10;
+	
+//metodit
+	public void jarruta() {
+		System.out.println("Auto jarruttaa");
+	}
+	
+	public void kiihdyta() {
+		System.out.println("Auto kiihtyy");
+		bensanMaara = (bensanMaara -1);
+	}
+	public void naytaTiedot() {
+		System.out.println("Merkki: " + merkki);
+		System.out.println("Malli: " + malli);
+		System.out.println("Bensan määrä: " + bensanMaara);
+	}
 }
